5fdc152b01dddd5e5932cf1560b542b658222db9
magzhan.karasayev <fallen.s4e@gmail.com>
2023-10-08 19:21:01 +0600

    Upd: targetChatId updated

--- src/test/scala/onyx/OnyxBotTest.scala
+++ src/test/scala/onyx/OnyxBotTest.scala
@@ -1,28 +1,37 @@
 package onyx
 
+import com.bot4s.telegram.api.declarative.Commands
+import com.bot4s.telegram.api.{AkkaTelegramBot, RequestHandler}
+import com.bot4s.telegram.clients.AkkaHttpClient
+import com.bot4s.telegram.future.Polling
+import com.bot4s.telegram.methods.ChatAction.ChatAction
+import com.bot4s.telegram.methods.{ChatAction, SendAnimation, SendChatAction, SendPhoto, SendVideo}
+import com.bot4s.telegram.models.{ChatId, InputFile, Message}
 import org.specs2.mutable.Specification
 import utils.FileService
 
 import java.io.File
-import scala.concurrent.Await
+import java.nio.file.Paths
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration.Duration
+
+import scala.concurrent.duration._
+
 
 class OnyxBotTest extends Specification {
 
   "OnyxBotTest" should {
     "run" in {
       val token = FileService.readFile(new File("/home/mark/.external-secrets/telegram-keys/onyx/token.txt"))
+      val mainChatId = "-1001891958439"
+      val targetChatId = "-1001845453886"
+
       val bot = new OnyxBot(
         token,
-        OnyxBotConfig("-1001891958439", "-4076585971"),
+        OnyxBotConfig(mainChatId, targetChatId),
       )
-
       val eol = bot.run()
-      println("Press [ENTER] to shutdown the bot, it may take a few seconds...")
-      scala.io.StdIn.readLine()
-      bot.shutdown() // initiate shutdown
-      // Wait for the bot end-of-life
-      Await.result(eol, Duration.Inf)
+      Thread.sleep(1000000)
 
       ok
     }
